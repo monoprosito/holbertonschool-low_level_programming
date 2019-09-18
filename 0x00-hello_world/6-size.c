752f97e7db81baa6783d0da4f75c83a6c4089ac6
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-09-18 23:03:12 +0000

    Adds the 6-size.c script

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the sizeof many var types
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long int: %d byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+	printf("Size of a float: %d byte(s)\n", sizeof(e));
+	return (0);
+}
