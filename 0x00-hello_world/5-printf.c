3039ec6dedff28d80f27526e6350c34f7272b852
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-09-18 22:21:56 +0000

    Adds the 5-printf.c script

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  * main - Prints a text
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
