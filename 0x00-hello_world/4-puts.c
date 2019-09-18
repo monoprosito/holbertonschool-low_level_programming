3debecabf1c1143e511b829b0a0f852b2f40da23
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-09-18 21:54:00 +0000

    Adds the 4-puts.c script

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  * main - Prints a text
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
