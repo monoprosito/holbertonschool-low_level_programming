4bb2309b435436055a748caa50130d689b2dd9a1
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-09-19 03:55:08 +0000

    Adds the 101-quote.c script

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+  * main - sss
+  *
+  * Return: sss
+  */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	return (1);
+}
