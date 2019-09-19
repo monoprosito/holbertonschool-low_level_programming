136d6b9f045dfb591dcdb632411b252eceda7907
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-09-19 04:21:06 +0000

    Fixes the 101-quote.c script

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
   */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 	return (1);
 }
