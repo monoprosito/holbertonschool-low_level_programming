ec2da50dd18f4d0b72f1b0313c94fd2a4967d392
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-09-19 22:37:04 +0000

    Fixes the 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
 
 /**
-  * main - Prints the alphabetic in lower and upper case
-  *
-  * Return: Always (Success)
-  */
+ * main - Prints the alphabetic in lower and upper case
+ *
+ * Return: Always (Success)
+ */
 int main(void)
 {
-	char c, d;
+	char c;
 
-	d = 'z';
-
-	for (c = 'a'; c <= d; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
+	}
 
-		if (c == 'z')
-		{
-			c = 'A';
-			d = 'Z';
-		}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
 	}
 
 	putchar('\n');
