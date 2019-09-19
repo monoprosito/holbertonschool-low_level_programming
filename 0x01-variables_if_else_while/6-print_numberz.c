f76355d4e4165870e94cdf6bd67c75dc5e54fc4f
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-09-19 23:47:06 +0000

    Adds the 6-print_numberz.c script

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main - ...
+  *
+  * Return: ...
+  */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
