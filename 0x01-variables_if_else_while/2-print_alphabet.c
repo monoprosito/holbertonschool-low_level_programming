503d30a252ddf453748787ba1b30c22b10c43164
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-09-19 21:48:22 +0000

    Adds the 2-print_alphabet.c script

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabetic
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}
+
