e4f5cc13a7f92ddac2c8f7931705ab8f8a5fe715
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-09-20 02:44:48 +0000

    Adds the 10-print_comb2.c

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the numbers from 00 to 99
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	int c, i;
+
+	c = i = '0';
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			putchar(c);
+			putchar(i);
+
+			if (i != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
