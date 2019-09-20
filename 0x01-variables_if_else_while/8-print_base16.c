52e3101ee0b36fec5a000a8389ac4ef155433656
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-09-20 00:37:58 +0000

    Adds the 8-print_base16.c script

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  * main - Prints a hexadecimal string
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
