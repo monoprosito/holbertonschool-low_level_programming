10216597bb8c6e69fe5126b8e23354d0930b3bce
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-09-24 16:26:54 +0000

    Fixes the 6-abs.c program

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,6 +2,7 @@
 
 /**
   * _abs - Computes the absolute value of an integer.
+  * @c: The number to be computed.
   *
   * Return: Absolute value of number or zero
   */
@@ -10,8 +11,10 @@ int _abs(int c)
 	if (c < 0)
 	{
 		int abs_val = c * -1;
+
 		return (abs_val);
 	}
 
 	return (0);
 }
+
