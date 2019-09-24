1f610283dd408086b7081145a86c13b1d92c3858
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-09-24 16:24:30 +0000

    Fixes the 5-sign program

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,8 +2,9 @@
 
 /**
   * print_sign - Prints the sign of a number
+  * @n: The number to be checked
   *
-  * Return: 1 for positive num, -1 for negative num or zero for anything else 
+  * Return: 1 for positive num, -1 for negative num or zero for anything else
   */
 int print_sign(int n)
 {
