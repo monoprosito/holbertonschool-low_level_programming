68b02b413d4f4b83fa7caabb0036bd0a6303c4c2
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-09-24 22:22:55 +0000

    Adds 10-add.c program

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+  * add - Adds two values
+  * @n1: The first number to be computed
+  * @n2: The second number to be computed
+  *
+  * Return: Integer value
+  */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
+
