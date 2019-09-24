a667c6a8008534d21cee23094eca5904667a39b0
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-09-24 00:03:34 +0000

    Adds program that prints the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+  * print_sign - Prints the sign of a number
+  *
+  * Return: 1 for positive num, -1 for negative num or zero for anything else 
+  */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -5,3 +5,4 @@
 3. 2-print_alphabet_x10.c: This program prints 10 times the alphabet, in lowercase, followed by a new line with a custom header.
 4. 3-islower.c: This program checks for lowercase character.
 5. 4-isalpha.c: This program checks for alphabetic character.
+6. 5-sign.c: This program prints the sign of a number.
