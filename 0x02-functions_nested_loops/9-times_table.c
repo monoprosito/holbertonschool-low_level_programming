ed1531b3282bb1113103a4d78d80595a8cbe9960
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-09-24 22:10:20 +0000

    Adds program to print the 9 times table, starting with 0

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+/**
+  * main - ...
+  *
+  * Return: ...
+  */
+void times_table(void)
+{
+	int x, y, z, u, d;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			z = x * y;
+
+			if (z > 9)
+			{
+				u = z % 10;
+				d = (z - u) / 10;
+
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
+			}
+			else
+			{
+				if (y != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+
+				_putchar(z + '0');
+			}
+		}
+
+		_putchar('\n');
+	}
+}
+
