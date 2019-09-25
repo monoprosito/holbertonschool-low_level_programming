01e28a4b8f551b9d4b1af039471dfd4ca1a25441
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-09-25 05:19:14 +0000

    Fixes the fourth test

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -24,7 +24,7 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if ((month == 2 && day == 60) || (month < 1 || month > 12) ||  day > 31)
+		if (month == 2 && day == 60)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
