65cf780b3aa26dc5c3048fa1cbb3b858593b8930
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-09-25 04:27:47 +0000

    Fixes the fourth test

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -21,7 +21,7 @@ void print_remaining_days(int month, int day, int year)
 
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
-    }
+	}
 	else
 	{
 		printf("Day of the year: %d\n", day);
