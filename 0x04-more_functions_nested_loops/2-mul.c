6233f8baddc7c3613021ab2650bcc1ae32eca954
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-09-26 15:01:06 +0000

    Adds a program to multiplies two integer numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+  * mul - Multiplies two integers
+  * @x: The first number to multiply
+  * @y: The second number to multiply with first number
+  *
+  * Return: The result of multiplication between x param and y param
+  */
+int mul(int x, int y)
+{
+	return (x * y);
+}
+
