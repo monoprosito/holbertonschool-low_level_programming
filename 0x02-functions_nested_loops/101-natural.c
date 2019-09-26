87db74af02c90ce5affdb26aee9c66ac46848201
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-09-26 03:58:57 +0000

    Adds the 101-natural.c program

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the sum of all multiples of 3 or 5 up to 1024
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	int i, z = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			z+= i;
+		}
+
+		i++;
+	}
+
+	printf("%d\n", z);
+	return (0);
+}
