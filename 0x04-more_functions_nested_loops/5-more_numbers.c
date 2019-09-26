59daaa1e8fc0f4518b15a23b0beec2114004101c
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-09-26 15:43:04 +0000

    Adds a program to print 10 times the numbers since 0 up to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+  * more_numbers - Print 10 times the numbers since 0 up to 14
+  *
+  * Return: 10 times of the numbers since 0 up to 14
+  */
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y > 9)
+			{
+				_putchar((y / 10) + '0');
+				_putchar((y % 10) + '0');
+			}
+			else
+			{
+				_putchar(y + '0');
+			}
+		}
+
+		_putchar('\n');
+	}
+}
+
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -4,3 +4,4 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
+void more_numbers(void);
