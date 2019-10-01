83e01461009fcbc3083ba75c6153fdb1e1dcd8c6
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-10-01 04:37:42 +0000

    Adds a program to return the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		c++;
+	}
+
+	return (c);
+}
+
