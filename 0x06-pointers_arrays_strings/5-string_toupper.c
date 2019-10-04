095150a867fa875d9cdaf8fbcd93a6c8ff890691
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-10-04 02:41:57 +0000

    Adds a program to changes all lowercase letters to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+  * string_toupper - Changes all lowercase letters to uppercase
+  * @p: The string will be modified
+  *
+  * Return: char var
+  */
+char *string_toupper(char *p)
+{
+	int a = 0;
+
+	while (p[a])
+	{
+		if (p[a] >= 97 && p[a] <= 122)
+		{
+			p[a] -= 32;
+		}
+
+		a++;
+	}
+
+	return (p);
+}
+
