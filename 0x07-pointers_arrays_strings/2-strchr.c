eaa0d9766eae0284692d3f857cccbaf7262bdeaf
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-10-07 17:34:08 +0000

    Adds a program to locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+  * _strchr - locate character in string
+  * @s: source string
+  * @c: character to find
+  *
+  * Return: the string from character found
+  */
+char *_strchr(char *s, char c)
+{
+	int a = 0, b, pos = 0;
+
+	while (s[a])
+	{
+		a++;
+	}
+
+	for (b = 0; b <= a; b++)
+	{
+		if (c == s[b])
+		{
+			s += pos;
+			return (s);
+		}
+		else
+		{
+			pos++;
+		}
+	}
+
+	return ('\0');
+}
+
