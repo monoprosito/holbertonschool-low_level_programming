05fe4ad9c53f64a66a879f8a60cb15f981f8d996
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-10-07 21:41:45 +0000

    Adds a program to searches a string for any of a set of bytes.

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+  * _strpbrk - search a string for any of a set of bytes
+  * @s: source string
+  * @accept: accepted characters
+  *
+  * Return: the string since the first found accepted character
+  */
+char *_strpbrk(char *s, char *accept)
+{
+	int a = 0, b;
+
+	while (s[a])
+	{
+		b = 0;
+
+		while (accept[b])
+		{
+			if (s[a] == accept[b])
+			{
+				s += a;
+				return (s);
+			}
+
+			b++;
+		}
+
+		a++;
+	}
+
+	return ('\0');
+}
+
