b345c0dd05560aac08b8e58a6808f3671efef3d9
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-10-07 15:02:15 +0000

    Adds a program to fills memory with a constant byte.

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+  * _memset - Fill memory with a constant byte
+  * @s: memory area to fill
+  * @b: constant byte to fill
+  * @n: bytes of memory area to fill
+  *
+  * Return: the memory area filled
+  */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		s[a] = b;
+	}
+
+	return (s);
+}
+
---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,2 @@
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
