82bc3e0cbc5e9318e9062c475541b7d39f7f63d0
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-10-09 03:22:50 +0000

    Adds a program to sets the value of a pointer to a char

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+  * set_string - Sets the value of a pointer to char
+  * @s: the value to modify
+  * @to: the value to assign
+  *
+  * Return: Nothing.
+  */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -6,3 +6,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
+void set_string(char **s, char *to);
