1badeef10e7d328ced1c0e4c2280b997f26e3be9
Santiago Arboleda L <monoprosito@gmail.com>
2019-10-12 14:54:42 -0500

    Fix some errors on holberton.h

--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -1,3 +1,5 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -18,3 +20,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+#endif
