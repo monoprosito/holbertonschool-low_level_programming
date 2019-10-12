7160e96b0580912b9ad386679bebc578ddc36195
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-10-12 23:31:44 +0000

    Adds a program to prints all args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+  * main - Prints all the args content
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: Always zero
+  */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
+
