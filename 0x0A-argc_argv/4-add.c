871bde346268a4cad89f90fe995e8272e482b9d8
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-10-13 19:32:54 +0000

    Fixes the 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,30 +12,29 @@
   */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
-	char *h;
+	int i;
+	unsigned int k, sum = 0;
+	char *e;
 
 	if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			h = argv[i];
+			e = argv[i];
 
-			while (*h)
+			for (k = 0; k < strlen(e); k++)
 			{
-				if (*h >= 48 && *h <= 57)
-				{
-					sum += atoi(h);
-				}
-				else
+				if (e[k] < 48 || e[k] > 57)
 				{
 					printf("Error\n");
 					return (1);
 				}
-				h++;
 			}
-			h++;
+
+			sum += atoi(e);
+			e++;
 		}
+
 		printf("%d\n", sum);
 	}
 	else
