d90d98b6526c3c5ca512741d6a8993019d7f7d67
Santiago Arboleda L <monoprosito@gmail.com>
2019-10-15 16:41:19 -0500

    Adds a program to concatenates two strings of any size

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * str_concat - Concatenates two strings of any size
+  * @s1: the first string to concatenate
+  * @s2: the second string to concatenate
+  *
+  * Return: the two strings concatenated
+  */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, j = 0, k = 0, l = 0;
+	char *s;
+
+	while (s1[i])
+	{
+		i++;
+	}
+
+	while (s2[j])
+	{
+		j++;
+	}
+
+	l = i + j;
+	s = malloc((sizeof(char) * l) + 1);
+	j = 0;
+
+	while (k < l)
+	{
+		if (k <= i)
+		{
+			s[k] = s1[k];
+		}
+
+		if (k >= i)
+		{
+			s[k] = s2[j];
+			j++;
+		}
+
+		k++;
+	}
+
+	s[k] = '\0';
+	return (s);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,2 +1,3 @@
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
