cc9dbf89e24ca706f882f5269838ff2fbf0d137e
Santiago Arboleda L <monoprosito@gmail.com>
2019-10-15 16:53:24 -0500

    If any parameter is null, the program treat the parameter as null

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,18 +14,17 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0, k = 0, l = 0;
 	char *s;
 
-	if (s1 == NULL || s2 == NULL)
-		return ("");
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
 
 	while (s1[i])
-	{
 		i++;
-	}
 
 	while (s2[j])
-	{
 		j++;
-	}
 
 	l = i + j;
 	s = malloc((sizeof(char) * l) + 1);
