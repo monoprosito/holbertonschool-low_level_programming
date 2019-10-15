ce17857b947c8b4c74d4ad81a036cc46e56b113c
Santiago Arboleda L <monoprosito@gmail.com>
2019-10-15 16:44:32 -0500

    Fix typo

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -28,16 +28,14 @@ char *str_concat(char *s1, char *s2)
 	s = malloc((sizeof(char) * l) + 1);
 
 	if (s == NULL)
-		return (NULL);	
+		return (NULL);
 
 	j = 0;
 
 	while (k < l)
 	{
 		if (k <= i)
-		{
 			s[k] = s1[k];
-		}
 
 		if (k >= i)
 		{
