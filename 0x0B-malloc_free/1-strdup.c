ca94dfacf6ac47ece5c4b27faa9e087d7f99e98c
Santiago Arboleda L <monoprosito@gmail.com>
2019-10-15 15:12:40 -0500

    Adds a program to copy the string given as a parameter to pointer

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+  * _strdup - ...
+  * @str: ...
+  *
+  * Return: char value
+  */
+char *_strdup(char *str)
+{
+	int a = 0, i = 1;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i])
+	{
+		i++;
+	}
+
+	s = malloc((sizeof(char) * i));
+
+	if (s == NULL)
+		return (NULL);
+
+	while (a < i)
+	{
+		s[a] = str[a];
+		a++;
+	}
+
+	s[a] = '\0';
+	return (s);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,1 +1,2 @@
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
