a0c0efc5af5d1f2bc9f2e03278d363a021de8beb
Santiago Arboleda L <monoprosito@gmail.com>
2019-10-15 19:31:19 -0500

    Revert "Fix the priority of the declaration of the two dimensional array"
    This reverts commit ef597bd1014293bdcc672fc704d84892a9eb13d3.

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,11 +11,12 @@
 int **alloc_grid(int width, int height)
 {
 	int i, j, k, l;
+	int **a;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	int **a = malloc(sizeof(int *) * height);
+	a = malloc(sizeof(int *) * height);
 
 	if (a == NULL)
 	{
