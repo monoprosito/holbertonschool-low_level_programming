24a7b14d1ceac9d4155de918a031496cb6c0ff51
Santiago Arboleda L <monoprosito@gmail.com>
2019-10-15 19:48:47 -0500

    Adds the prototype of the 4-free_grid.c task

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -2,3 +2,4 @@ char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
