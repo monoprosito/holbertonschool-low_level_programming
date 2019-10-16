ef597bd1014293bdcc672fc704d84892a9eb13d3
Santiago Arboleda L <monoprosito@gmail.com>
2019-10-15 19:25:19 -0500

    Fix the priority of the declaration of the two dimensional array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,12 +11,11 @@
 int **alloc_grid(int width, int height)
 {
 	int i, j, k, l;
-	int **a;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	a = malloc(sizeof(int *) * height);
+	int **a = malloc(sizeof(int *) * height);
 
 	if (a == NULL)
 	{
