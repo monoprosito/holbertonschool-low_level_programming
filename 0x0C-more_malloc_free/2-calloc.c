e18e7e0430cb84f94a04063c57d697d3b4a4c27c
Santiago Arboleda L <monoprosito@gmail.com>
2019-10-17 13:59:13 -0500

    Adds a program to allocates memory for an array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+  * _calloc - ...
+  * @nmemb: number of members
+  * @size: size
+  *
+  * Return: ...
+  */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int i = 0, l = 0;
+	int *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	l = nmemb * size;
+	p = malloc(l);
+
+	if (p == NULL)
+		return (NULL);
+
+	while (i < l)
+	{
+		p[i] = 0;
+		i++;
+	}
+
+	return (p);
+}
