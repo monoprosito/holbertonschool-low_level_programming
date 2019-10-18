26b15f42c4608d6a297780535163b01d5fd4e7e4
Santiago Arboleda L <monoprosito@gmail.com>
2019-10-18 17:13:31 -0500

    Completes the algorithm

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,11 +11,12 @@
   */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *nptr;
+	int *nptr;
+	unsigned int i;
 
 	if (new_size == old_size)
 		return (ptr);
-		
+
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
@@ -23,5 +24,28 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 
 	if (ptr == NULL)
+	{
 		nptr = malloc(new_size);
+
+		if (nptr == NULL)
+			return (NULL);
+
+		return (nptr);
+	}
+
+	if (new_size > old_size)
+	{
+		nptr = malloc(new_size);
+
+		if (nptr == NULL)
+			return (NULL);
+
+		for (i = 0; i <= old_size; i++)
+		{
+			nptr[i] = ((int *) ptr)[i];
+		}
+	}
+
+	free(ptr);
+	return (nptr);
 }
