ac33517c5cc3338ecfc7031b841a010340b8af02
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-10-19 18:47:17 +0000

    Adds a program to encodes a string using rot13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+  * rot13 - ...
+  * @s: ...
+  *
+  * Return: ...
+  */
+char *rot13(char *s)
+{
+	int a = 0;
+
+	while (s[a])
+	{
+		while ((s[a] >= 'a' && s[a] <= 'z') || (s[a] >= 'A' && s[a] <= 'Z'))
+		{
+			if ((s[a] > 'm' && s[a] <= 'z') || (s[a] > 'M' && s[a] <= 'Z'))
+				s[a] -= 13;
+			else
+				s[a] += 13;
+
+			break;
+		}
+
+		a++;
+	}
+
+	return (s);
+}
+
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -7,3 +7,4 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+char *rot13(char *);
