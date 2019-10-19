abc8d247b9b165524a2a447b98fbaaa77e391bb8
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-10-19 19:51:13 +0000

    Fix the algorithm to sum each diagonal

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,13 +11,14 @@
   */
 void print_diagsums(int *a, int size)
 {
-	int b, sum1 = 0;
+	int b, sum1 = 0, sum2 = 0;
 
 	for (b = 0; b < size; b++)
 	{
 		sum1 += a[(size + 1) * b];
+		sum2 += a[(size - 1) * (b + 1)];
 	}
 
-	printf("%d, \n", sum1);
+	printf("%d, %d\n", sum1, sum2);
 }
 
