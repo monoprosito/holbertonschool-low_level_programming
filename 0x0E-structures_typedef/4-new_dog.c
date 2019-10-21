82fbe5a1408b0740c917bf51c05d7e27b7fcdc8a
Santiago Arboleda L <monoprosito@gmail.com>
2019-10-21 18:41:20 -0500

    Creates a cutie dog structure

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+  * new_dog - ...
+  * @name: ...
+  * @age: ...
+  * @owner: ...
+  *
+  * Return: ... 
+  */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *cutie_dog;
+	int name_l = 0, own_l = 0;
+
+	if (name != NULL && owner != NULL)
+	{
+		name_l = strlen(name);
+		own_l = strlen(owner);
+		cutie_dog = malloc(sizeof(dog_t));
+
+		if (cutie_dog == NULL)
+			return (NULL);
+
+		cutie_dog->name = malloc(sizeof(char) * name_l);
+
+		if (cutie_dog->name == NULL)
+		{
+			free(cutie_dog);
+			return (NULL);
+		}
+
+		cutie_dog->owner = malloc(sizeof(char) * own_l);
+
+		if (cutie_dog->owner == NULL)
+		{
+			free(cutie_dog->name);
+			free(cutie_dog);
+			return (NULL);
+		}
+
+		cutie_dog->name = strcpy(cutie_dog->name, name);
+		cutie_dog->owner = strcpy(cutie_dog->owner, owner);
+		cutie_dog->age = age;
+	}
+
+	return (cutie_dog);
+}
