af845e88012f68b8386a6314961565d1452601b1
Santiago Arboleda L <monoprosito@gmail.com>
2019-10-21 18:50:02 -0500

    Adds own functions of strlen and strcpy without std libs

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,8 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
+
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 
 /**
   * new_dog - ...
@@ -18,8 +19,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (name != NULL && owner != NULL)
 	{
-		name_l = strlen(name);
-		own_l = strlen(owner);
+		name_l = _strlen(name);
+		own_l = _strlen(owner);
 		cutie_dog = malloc(sizeof(dog_t));
 
 		if (cutie_dog == NULL)
@@ -42,10 +43,49 @@ dog_t *new_dog(char *name, float age, char *owner)
 			return (NULL);
 		}
 
-		cutie_dog->name = strcpy(cutie_dog->name, name);
-		cutie_dog->owner = strcpy(cutie_dog->owner, owner);
+		cutie_dog->name = _strcpy(cutie_dog->name, name);
+		cutie_dog->owner = _strcpy(cutie_dog->owner, owner);
 		cutie_dog->age = age;
 	}
 
 	return (cutie_dog);
+}
+
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		c++;
+	}
+
+	return (c);
+}
+
+/**
+  * _strcpy - Copy a string
+  * @dest: Destination value
+  * @src: Source value
+  *
+  * Return: the pointer to dest
+  */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i++] = '\0';
+
+	return (dest);
 }
