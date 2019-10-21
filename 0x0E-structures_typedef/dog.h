fc3a03234e6254bb063c6b569222cb34931652ba
Santiago Arboleda L <monoprosito@gmail.com>
2019-10-21 15:39:42 -0500

    Adds the structure of a cutie dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+/**
+  * struct dog - Dog attributes
+  * @name: The name of the dog.
+  * @age: The age of the dog.
+  * @owner: The owner of the dog.
+  *
+  * Description: The attributes of a dog.
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
