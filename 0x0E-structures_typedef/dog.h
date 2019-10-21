1490b56e2c24d7abe78d56e962a4e7698c3f5e4e
Santiago Arboleda L <monoprosito@gmail.com>
2019-10-21 16:49:50 -0500

    Adds a new typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,6 +16,11 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - Typedef for dog structure
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
