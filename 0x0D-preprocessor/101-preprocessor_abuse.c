cc70348d2aefcde347866965c63afbdb08bc4d62
Santiago Arboleda L <monoprosito@gmail.com>
2019-10-21 19:22:04 -0500

    Prints a text with macros

---
+++ 0x0D-preprocessor/101-preprocessor_abuse.c
@@ -0,0 +1,16 @@
+#ifndef _HEADER_
+#define _HEADER_
+#define TEXT "Hello, Holberton"
+#endif
+#include <stdio.h>
+
+/**
+  * main - ...
+  *
+  * Return: ...
+  */
+int main(void)
+{
+	printf("%s\n", TEXT);
+	return (0);
+}
