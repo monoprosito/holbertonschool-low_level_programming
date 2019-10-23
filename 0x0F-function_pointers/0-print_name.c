900281ee20f5fabeb099377410d1e7b90b9e1954
Santiago Arboleda L <monoprosito@gmail.com>
2019-10-23 11:22:59 -0500

    Prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+  * print_name - Prints a name
+  * @name: The name to prints
+  * @f: Pointer to function
+  *
+  * Return: Nothing
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	void (*ptrtf)(char *) = f;
+
+	ptrtf(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,4 @@
+#ifndef _HEADER_
+#define _HEADER_
+void print_name(char *name, void (*f)(char *));
+#endif
