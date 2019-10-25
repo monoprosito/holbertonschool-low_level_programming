14375edd422abe0dee13521e904c0238bcdccd7c
Santiago Arboleda L <monoprosito@gmail.com>
2019-10-25 14:50:29 -0500

    Fixes the output when separator is NULL

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,7 +15,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i = 0;
 	char *string;
 
-	if (separator != NULL && n != 0)
+	if (n != 0)
 	{
 		va_start(args, n);
 
@@ -27,7 +27,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			else
 				printf("%s", string);
 
-			if (i != n - 1)
+			if (i != n - 1 && separator != NULL)
 				printf("%s", separator);
 
 			i++;
