cea4c5e14bce74082b7f8c9a0b222f6cea1b0d6f
Santiago Arboleda L <monoprosito@gmail.com>
2019-10-25 10:43:21 -0500

    Computes the sum of all its params

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+  * sum_them_all - ...
+  * @n: ...
+  *
+  * Return: ...
+  */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i = 0;
+	int sum = 0;
+
+	if (n != 0)
+	{
+		va_start(args, n);
+
+		while (i < n)
+		{
+			sum += va_arg(args, int);
+			i++;
+		}
+
+		va_end(args);
+		return (sum);
+	}
+
+	return (0);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,5 @@
+#ifndef _HEADER_
+#define _HEADER_
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+#endif
