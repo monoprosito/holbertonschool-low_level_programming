4220d3c2b524722c1f5493f605187d3e846347eb
Santiago Arboleda L <monoprosito@gmail.com>
2019-10-29 15:09:57 -0500

    Prints any number

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+  * print_number - Prints any integer with putchar
+  * @n: Number to prints
+  *
+  * Return: Nothing
+  */
+void print_number(int n)
+{
+	int x;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+	
+	x = n;
+
+	if (x / 10)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
+}
