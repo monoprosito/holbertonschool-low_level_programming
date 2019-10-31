7f85c6fc3eff1f6bad2e4c9bc7a350514e36d329
Santiago Arboleda L <monoprosito@gmail.com>
2019-10-31 12:44:01 -0500

    Adds the basic header

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,24 @@
+#ifndef _LINKED_LIST
+#define _LINKED_LIST
+
+#include <stdlib.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif
