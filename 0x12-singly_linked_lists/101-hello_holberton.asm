2baaff0f8cafadd671168eabd6b2e2046ec1224d
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-11-02 12:41:58 -0500

    Prints Hello, Holberton with Assembly

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,17 @@
+section .text
+global main
+
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, msglen
+	syscall
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
+
+section .rodata
+	msg: db "Hello, Holberton", 10
+	msglen: equ $ - msg
