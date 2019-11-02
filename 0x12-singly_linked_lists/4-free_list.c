c06e9f649b262a17a63d62af5290f00c9eadfa91
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-11-02 00:02:53 -0500

    Frees a linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+  * free_list - Frees a linked list.
+  * @head: The pointer to the first node of linked list
+  *
+  * Return: Nothing
+  */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		free(head);
+		head = temp->next;
+		free(temp);
+	}
+}
