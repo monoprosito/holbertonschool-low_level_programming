7e5234690594b7f09fec950b596f3e5267d0c8e6
Santiago Arboleda L <monoprosito@gmail.com>
2019-11-04 12:16:37 -0500

    Frees a linked list that was passed as double pointer

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+  * free_listint2 - ...
+  * @head: ...
+  *
+  * Return: ...
+  */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head)
+	{
+		temp = (*head);
+		*head = (*head)->next;
+		free(temp);
+	}
+
+	free(*head);
+	head = 0;
+}
