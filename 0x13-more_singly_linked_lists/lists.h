b06fc25069b2157afbb6ed66767a9d5a1be2ab83
Santiago Arboleda L <monoprosito@gmail.com>
2019-11-04 16:40:30 -0500

    Adds the prototype of the 10 exercise

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -25,5 +25,6 @@ int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
 #endif
