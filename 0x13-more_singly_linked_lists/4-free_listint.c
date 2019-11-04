fcedcc9550c7d1be9c1194f1ee57b120df768932
Santiago Arboleda L <monoprosito@gmail.com>
2019-11-04 11:15:18 -0500

    Frees a linked list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+  * free_listint - Frees a linked list
+  * @head: The head of the linked list
+  *
+  * Return: Nothing
+  */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+
+	free(head);
+}
