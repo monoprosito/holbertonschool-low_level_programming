ac14bf749c3e88c38e5e8d17c7cb608509ea5cf0
Santiago Arboleda L <monoprosito@gmail.com>
2019-11-05 09:35:33 -0500

    Removes a unnecessary conditional

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,7 +13,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_list, *temp;
 
-	if (head && n >= 0)
+	if (head)
 	{
 		new_list = malloc(sizeof(listint_t));
 		if (new_list == NULL)
