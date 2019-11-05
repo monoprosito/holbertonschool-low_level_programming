21b1f2defc1c7ac414d4fabc90539f3ee948f6de
Santiago Arboleda L <monoprosito@gmail.com>
2019-11-05 14:26:37 -0500

    Fixes the address to point when the index is zero

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,8 +27,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			if (count == idx)
 			{
-				new_node->next = temp;
-				prev->next = new_node;
+				if (idx > 0)
+				{
+					new_node->next = temp;
+					prev->next = new_node;
+				}
+				else
+				{
+					new_node->next = *head;
+					*head = new_node;
+				}
+
 				return (new_node);
 			}
 
