fcb1b1b2dbb7e2309adf889fb39fa705e73f7355
Santiago Arboleda L <monoprosito@gmail.com>
2019-11-05 10:38:27 -0500

    Check if the pointer-to-pointer is not null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,11 +11,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (*head)
+	if (head)
 	{
-		temp = (*head);
-		*head = (*head)->next;
-		free(temp);
+		while (*head)
+		{
+			temp = (*head);
+			*head = (*head)->next;
+			free(temp);
+		}
 	}
 
 	free(*head);
