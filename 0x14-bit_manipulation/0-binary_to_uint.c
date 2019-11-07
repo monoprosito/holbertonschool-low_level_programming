fd748eb5e8396f8eb0b20614f697f1297b001096
Santiago Arboleda L <monoprosito@gmail.com>
2019-11-07 15:17:30 -0500

    Converts a binary number to an unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+  * binary_to_uint - Converts a binary number to an unsigned int
+  * @b: The binary string to converts
+  *
+  * Return: The positive number converted from a binary
+  */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int len = 0, count = 0, sum = 0;
+
+	if (b == NULL)
+		return (0);
+
+	len = _strlen(b);
+	while (len--)
+	{
+		if (b[len] != 48 && b[len] != 49)
+			return (0);
+
+		if (b[len] == 49)
+			sum += 1 << count;
+
+		count++;
+	}
+
+	return (sum);
+}
+
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(const char *s)
+{
+	int c = 0;
+
+	while (s[c])
+		c++;
+
+	return (c);
+}
---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,7 @@
+#ifndef _BIT_MANIP_
+#define _BIT_MANIP_
+
+int _strlen(const char *s);
+unsigned int binary_to_uint(const char *b);
+
+#endif
