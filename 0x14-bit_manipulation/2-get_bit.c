47bc24c5702df6dd609f646a54da10273cc5cb95
Santiago Arboleda L <monoprosito@gmail.com>
2019-11-07 19:06:24 -0500

    Gets the value of a bit at a given index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+  * get_bit - Gets the value of a bit at a given index
+  * @n: The number to find the index
+  * @index: The index to find
+  *
+  * Return: ...
+  */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int comp_i = 0;
+
+	while (n)
+	{
+		if (comp_i == index)
+		{
+			if (n % 2)
+				return (1);
+			else
+				return (0);
+		}
+
+		n = n / 2;
+		comp_i++;
+	}
+
+	return (-1);
+}
--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -6,5 +6,6 @@ unsigned int binary_to_uint(const char *b);
 int _putchar(char c);
 void print_binary(unsigned long int n);
 void _divide(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
 
 #endif
