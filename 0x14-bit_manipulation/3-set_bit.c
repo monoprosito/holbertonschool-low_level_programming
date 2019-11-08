89b2e90e76fb304ead79a7092e2ccd3fbfc88ce2
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-11-07 22:18:37 -0500

    Sets the value of a bit to 1 at a given index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+  * set_bit - Sets the value of a bit to 1 at a given index
+  * @n: The number to modify
+  * @index: The index in the number to modify
+  *
+  * Return: 1 if it worked, or -1 if an error occurred
+  */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	return ((*n |= 1 << index) ? 1 : -1);
+}
--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -7,5 +7,6 @@ int _putchar(char c);
 void print_binary(unsigned long int n);
 void _divide(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 
 #endif
