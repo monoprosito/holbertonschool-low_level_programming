75be490df8cc373c30e4d67cad875941da1d4998
Santiago Arboleda L <monoprosito@gmail.com>
2019-11-08 15:36:04 -0500

    Fixes the returned value

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,8 +11,11 @@
   */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > sizeof(unsigned long int) * 8)
-		return (-1);
+	if (index < sizeof(unsigned long int) * 8)
+	{
+		*n &= (~(1 << index));
+		return (1);
+	}
 
-	return ((*n &= ~(1 << index)) ? 1 : -1);
+	return (-1);
 }
