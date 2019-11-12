c74fc905a0f4dd8262088004332430bca50a9683
Santiago Arboleda L <monoprosito@gmail.com>
2019-11-12 15:23:47 -0500

    Test a case

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,12 +15,13 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND, 0664);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 
-	if (text_content && write(fd, text_content, _strlen(text_content)) != -1)
+	if (text_content)
 	{
+		write(fd, text_content, _strlen(text_content));
 		close(fd);
 		return (1);
 	}
