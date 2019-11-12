8cf916ad510ea5ce540f215d110c9c130e0a951e
Santiago Arboleda L <monoprosito@gmail.com>
2019-11-12 15:28:23 -0500

    Fixes some validation errors

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,13 +21,12 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		write(fd, text_content, _strlen(text_content));
-		close(fd);
-		return (1);
+		if (write(fd, text_content, _strlen(text_content)) == -1)
+			return (-1);
 	}
 
 	close(fd);
-	return (-1);
+	return (1);
 }
 
 /**
