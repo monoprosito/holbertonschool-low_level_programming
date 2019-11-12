6f8970ec749ca9026005a23aff7d1dfc241e32c6
Santiago Arboleda L <monoprosito@gmail.com>
2019-11-12 14:51:15 -0500

    Appends text at the end of a file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+
+/**
+  * append_text_to_file - ...
+  * @filename: ...
+  * @text_content: ...
+  *
+  * Return: ...
+  */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content && write(fd, text_content, _strlen(text_content)) != -1)
+	{
+		close(fd);
+		return (1);
+	}
+
+	close(fd);
+	return (-1);
+}
+
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c])
+		c++;
+
+	return (c);
+}
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -11,5 +11,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int _strlen(char *s);
 int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif
