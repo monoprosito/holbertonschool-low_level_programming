9a762eb5e0a495eb3996f6022f51fb32bbe8a5c9
Santiago Arboleda L <monoprosito@gmail.com>
2019-11-12 20:13:45 -0500

    Fixes an error case at reading

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -40,19 +40,20 @@ int copy_file(const char *src, const char *dest)
 	}
 
 	tfd = open(dest, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (tfd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest);
-		exit(99);
-	}
 
 	while ((readed = read(ofd, buff, 1024)) > 0)
 	{
-		if (write(tfd, buff, readed) == -1)
+		if (write(tfd, buff, readed) != readed || tfd == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest);
 			exit(99);
 		}
+	}
+
+	if (readed == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
+		exit(98);
 	}
 
 	if (close(ofd) == -1)
