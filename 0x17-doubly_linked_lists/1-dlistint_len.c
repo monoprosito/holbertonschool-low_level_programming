d53a8fb9593b863ea5d582e27bb4f49346c62243
Santiago Arboleda L <monoprosito@gmail.com>
2019-12-13 15:37:30 -0500

    Counts the number of nodes in a doubly linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+  * dlistint_len - Counts the number of elements in a doubly linked list
+  * @h: The double linked list to count
+  *
+  * Return: Number of elements in the doubly linked list
+  */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int lenght = 0;
+
+	while (h != NULL)
+	{
+		++lenght;
+		h = h->next;
+	}
+
+	return (lenght);
+}
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -18,5 +18,6 @@ typedef struct dlistint_s
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
 
 #endif
