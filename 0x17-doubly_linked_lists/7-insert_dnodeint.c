78c7b8eb0602fea5e0896b6d222a9449f17f527b
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-12-14 17:40:18 -0500

    Fixes the elements added to the final

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -24,7 +24,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	length = dlistint_len(*h);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	else if (length == idx)
+	else if (length == idx || length + 1 == idx)
 		return (add_dnodeint_end(h, n));
 
 	current = *h;
