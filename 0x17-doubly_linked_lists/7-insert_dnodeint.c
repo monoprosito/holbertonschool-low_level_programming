3ae3957c72e0913defa62059d844e7345ffae628
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-12-14 15:31:48 -0500

    Allows add a new node in all positions, including a empty doubly linked list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -6,35 +6,72 @@
   * insert_dnodeint_at_index - Inserts a new node at a given position
   * @h: The head of the doubly linked list
   * @idx: The index in which insert the new node
+  * @n: The number to insert in the new node
   *
   * Return: The address of the new node, or NULL if it failed
   */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current = *h, *new_node = NULL;
+	dlistint_t *current = NULL, *new_node = NULL;
 	unsigned int iter_times = 0;
-	
-	if (current)
+
+	if (h)
 	{
-		new_node = malloc(sizeof(dlistint_t));
-		if (new_node == NULL)
-			return (NULL);
-
-		new_node->n = n;
-		while (current != NULL)
+		current = *h;
+		if (current)
 		{
-			if (iter_times == idx)
+			while (current != NULL)
 			{
-				new_node->next = current;
-				new_node->prev = current->prev;
-				current->prev = new_node;
-				return (new_node);
-			}
+				if (iter_times == idx)
+				{
+					new_node = create_node(n, current, current->prev);
+					current->prev = new_node;
+					current = new_node;
+					if (idx == 0)
+						*h = new_node;
+					else
+						current->prev->next = new_node;
+					return (new_node);
+				}
+				++iter_times;
 
-			current = current->next;
-			++iter_times;
+				if (iter_times == idx - 1)
+				{
+					new_node = create_node(n, current->next, current);
+					current->next = new_node;
+					return (new_node);
+				}
+
+				current = current->next;
+			}
 		}
+
+		new_node = create_node(n, NULL, NULL);
+		*h = new_node;
+		return (new_node);
 	}
 
 	return (NULL);
+}
+
+/**
+  * create_node - Create a new node with values
+  * @n: The number of the new node
+  * @next: The next node of the new node
+  * @prev: The previous node of the new node
+  *
+  * Return: The address of the new node created
+  */
+dlistint_t *create_node(unsigned int n, void *next, void *prev)
+{
+	dlistint_t *new_node = NULL;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = next;
+	new_node->prev = prev;
+	return (new_node);
 }
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -25,5 +25,6 @@ void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+dlistint_t *create_node(unsigned int n, void *next, void *prev);
 
 #endif
