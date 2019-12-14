1518313b59d22bc18afb1a47a855e5c4cb82cb06
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-12-14 13:43:24 -0500

    Fixes the value of the prev in each node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -19,6 +19,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 
 	new_node->n = n;
+	if (*head)
+	{
+		new_node->next = *head;
+		new_node->prev = (*head)->prev;
+		(*head)->prev = new_node;
+		*head = new_node;
+		return (*head);
+	}
+
 	new_node->next = *head;
 	new_node->prev = NULL;
 	*head = new_node;
