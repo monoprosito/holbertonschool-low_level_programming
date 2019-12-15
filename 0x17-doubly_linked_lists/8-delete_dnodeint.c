9c82b0e23d0a135d2d86977d918379f3536afc25
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-12-15 12:08:06 -0500

    Fixes the deletion of the last node and refactor

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -12,42 +12,34 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *current = NULL, *temp = NULL;
-	unsigned int iter_times = 0, length = 0;
+	unsigned int length = 0;
 
 	if (head && *head)
 	{
-		current = *head;
-		length = dlistint_len(current);
-
+		length = dlistint_len(*head);
 		if (index > length)
 			return (-1);
 
 		if (index == 0)
 			return (delete_first_dnode(head));
 
-		if (length == index)
+		current = get_dnodeint_at_index(*head, index);
+		if (current)
 		{
-			current = get_dnodeint_at_index(*head, index - 1);
 			temp = current;
-			current->prev->next = current->next;
+			if (length - 1 == index)
+				current->prev->next = current->next;
+			else
+			{
+				current->prev->next = current->next;
+				current->next->prev = current->prev;
+			}
+
 			free(temp);
 			return (1);
 		}
-
-		while (current != NULL)
-		{
-			if (iter_times == index)
-			{
-				temp = current;
-				current->prev->next = current->next;
-				current->next->prev = current->prev;
-				free(temp);
-				return (1);
-			}
-			current = current->next;
-			++iter_times;
-		}
 	}
+
 	return (-1);
 }
 
