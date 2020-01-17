459fa5aace29b79a879e2aefeedb52b4adfd25ec
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-01-17 16:27:40 -0500

    Creates the scratch header for the project

---
+++ 0x1A-hash_tables/hash_tables.h
@@ -0,0 +1,33 @@
+#ifndef _HASH_T
+#define _HASH_T
+
+/**
+ * struct hash_node_s - Node of a hash table
+ *
+ * @key: The key, string
+ * The key is unique in the HashTable
+ * @value: The value corresponding to a key
+ * @next: A pointer to the next node of the List
+ */
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
+
+/**
+ * struct hash_table_s - Hash table data structure
+ *
+ * @size: The size of the array
+ * @array: An array of size @size
+ * Each cell of this array is a pointer to the first node of a linked list,
+ * because we want our HashTable to use a Chaining collision handling
+ */
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	hash_node_t **array;
+} hash_table_t;
+
+#endif
