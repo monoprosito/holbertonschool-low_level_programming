afd783214340502150745d7ad1857b3ccdfdad09
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-01-18 15:11:43 -0500

    Creates a hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+  * hash_table_create - Creates a hash table
+  * @size: Size of the array
+  *
+  * Return: A pointer to the newly created hash table
+  */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *htable = NULL;
+	hash_node_t **arr = NULL;
+
+	htable = malloc(sizeof(hash_table_t));
+	if (htable == NULL)
+		return (NULL);
+
+	*arr = malloc(sizeof(hash_node_t) * size);
+	if (*arr == NULL)
+		return (NULL);
+
+	htable->size = size;
+	htable->array = arr;
+
+	return htable;
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -30,4 +30,6 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
+hash_table_t *hash_table_create(unsigned long int size);
+
 #endif
