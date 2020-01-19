17b864d9fd7f726f4446068a3fdafe64cc8bb0b3
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-01-19 14:53:19 -0500

    Replaces the value of the key, if the key already exists

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,23 +19,22 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL || key == NULL || (strcmp(key, "") == 0))
 		return (0);
 
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
-		return (0);
-
 	idx = key_index((unsigned char *) key, ht->size);
 	elem = ht->array[idx];
 
 	if (elem && strcmp(key, elem->key) == 0)
 	{
-		free(elem);
-		ht->array[idx] = NULL;
+		elem->value = strdup(value);
+		return (1);
 	}
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
 
 	new_node->key = (char *) key;
 	new_node->value = strdup(value);
-	new_node->next = ht->array[idx];
-	ht->array[idx] = new_node;
-
+	new_node->next = elem;
+	elem = new_node;
 	return (1);
 }
