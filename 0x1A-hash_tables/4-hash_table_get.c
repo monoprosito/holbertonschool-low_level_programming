80ad3e894a3e64f33d6bf098b8a8b2522edc7bd3
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-01-18 19:18:11 -0500

    Retrieves a value associated with a key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+  * hash_table_get - Retrieves a value associated with a key
+  * @ht: The hash table to look into
+  * @key: The key to finding
+  *
+  * Return: The value associated with the element
+  * or NULL if key couldn't be found
+  */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int idx = 0;
+	hash_node_t *elem = NULL;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	idx = key_index((unsigned char *) key, ht->size);
+	elem = ht->array[idx];
+
+	if (elem == NULL)
+		return (NULL);
+
+	while (key != elem->key)
+		elem = elem->next;
+
+	return (elem->value);
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -34,5 +34,6 @@ hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
 
 #endif
