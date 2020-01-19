46a3753a4968c4ffbdb810ec81baa1955d812b18
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-01-19 16:21:53 -0500

    Prints a hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+  * hash_table_print - Prints a hash table
+  * @ht: The hash table to prints
+  *
+  * Return: Nothing!
+  */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node = NULL;
+	unsigned long int i = 0;
+	short pr_commas = 0;
+
+	if (ht == NULL)
+	{
+		printf("{}\n");
+		return;
+	}
+
+	printf("{");
+
+	for (; i < ht->size; ++i)
+	{
+		node = ht->array[i];
+
+		while (node)
+		{
+			if (pr_commas == 1)
+				printf(", ");
+
+			pr_commas = 1;
+			printf("'%s': '%s'", (char *) node->key,
+								(char *) node->value);
+
+			node = node->next;
+		}
+	}
+
+	printf("}\n");
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -35,5 +35,6 @@ unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
 
 #endif
