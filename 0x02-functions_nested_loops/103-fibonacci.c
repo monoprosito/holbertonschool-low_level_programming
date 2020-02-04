877cdfcf34e21f01f1239aaf509b14a5aaf59417
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-02-03 21:41:41 -0500

    Prints the sum of the Even Fibonacci Numbers

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the sum of Even Fibonacci numbers
+  * less than 4000000.
+  *
+  * Return: Nothing!
+  */
+int main(void)
+{
+	int i = 0;
+	long j = 1, k = 2, sum = 0;
+
+	while (k + j < 4000000)
+	{
+		k += j;
+
+		if (k % 2 == 0)
+			sum += k;
+
+		j = k - j;
+
+		++i;
+	}
+
+	printf("%ld\n", sum);
+	return (0);
+}
