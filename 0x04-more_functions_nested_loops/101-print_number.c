dd7c0048f41a80b59fe9e27b95db4b7fc226c9b0
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-03-17 15:00:11 -0500

    Fixes some numbers that can't be displayed

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,16 +8,16 @@
   */
 void print_number(int n)
 {
-	unsigned int k = 0;
+	unsigned int k = n;
 
 	if (n < 0)
-		n *= -1, _putchar('-');
+		k = n * -1, _putchar('-');
 
-	k = n / 10;
+	k /= 10;
 
 	if (k != 0)
 		print_number(k);
 
-	_putchar(n % 10 + '0');
+	_putchar((unsigned int) n % 10 + '0');
 
 }
