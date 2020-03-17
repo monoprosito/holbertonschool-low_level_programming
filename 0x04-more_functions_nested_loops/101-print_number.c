213da5efcff7a9cc2a42f1098af5ba8dbbcf0ebd
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-03-17 16:55:43 -0500

    Fixes some negative numbers that can't be displayed

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
   * print_number - Prints an integer.
@@ -11,7 +12,11 @@ void print_number(int n)
 	unsigned int k = n;
 
 	if (n < 0)
-		k = n * -1, _putchar('-');
+	{
+		n *= -1;
+		k = n;
+		_putchar('-');
+	}
 
 	k /= 10;
 
