78211bc31a28e00dca7c7337dccc6eb48ddc5d08
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-04-15 12:25:31 -0500

    Searches for a value in an array of integers
    This script is using the Linear search algorithm

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,33 @@
+#include "search_algos.h"
+#include <stdio.h>
+
+/**
+  * linear_search - Searches for a value in an array of
+  * integers using the Linear search algorithm
+  *
+  * @array: A pointer to the first element of the array to search in
+  * @size: The number of elements in array
+  * @value: The value to search for
+  *
+  * Return: The first index where value is located
+  */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (array == NULL)
+		return (-1);
+
+	for (; i < size; ++i)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+
+		if (array[i] == value)
+			break;
+	}
+
+	if (i == size)
+		return (-1);
+
+	return (i);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,8 @@
+#ifndef _BINARY_SEARCH_
+#define _BINARY_SEARCH_
+
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+
+#endif
