2dfd97806a62b2a7c1f11379808f60f784c1e359
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-04-15 20:21:47 -0500

    Adds the stdio.h in the header

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,5 +1,4 @@
 #include "search_algos.h"
-#include <stdio.h>
 
 /**
   * linear_search - Searches for a value in an array of
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,7 +2,9 @@
 #define _BINARY_SEARCH_
 
 #include <stdlib.h>
+#include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 
 #endif
